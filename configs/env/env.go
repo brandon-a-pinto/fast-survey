68cd39879599640e31f01676dd895938d5613607
Brandon Pinto <brandon.a.pinto@protonmail.com>
2023-07-26 14:10:43 -0300

    chore: remove duplicate .env example

--- configs/env/env.go
+++ configs/env/env.go
@@ -2,16 +2,13 @@ package env
 
 import (
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func SetupEnv() {
-	if os.Getenv("APP_ENV") != "production" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
 }
